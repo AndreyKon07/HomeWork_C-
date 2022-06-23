149fb918b3fffd74a427ca7080eb18c597076b63
AndreyKon07 <107915506+AndreyKon07@users.noreply.github.com>
2022-06-23 16:22:39 +0300

    TASK2

---
+++ T2Program.cs
@@ -0,0 +1,16 @@
+﻿int a, b;
+
+Console.WriteLine("Input first number");
+a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input first number");
+b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(b);
+}

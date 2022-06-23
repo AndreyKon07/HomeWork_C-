dedc2d8ad3ad53031219c9eff283fb4c3f4eff2f
AndreyKon07 <107915506+AndreyKon07@users.noreply.github.com>
2022-06-23 16:24:04 +0300

    TASK8

---
+++ T8Program.cs
@@ -0,0 +1,23 @@
+﻿int i=1;
+
+Console.WriteLine("Input number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+do
+
+{
+if (i % 2 == 0)
+{
+    Console.WriteLine(i);
+    
+}
+i++;
+}
+while (i <= a);
+
+
+ 
+
+
+
+

11d406155b73c014df3ed0c7429aa7a146a93219
AndreyKon07 <107915506+AndreyKon07@users.noreply.github.com>
2022-06-29 17:11:47 +0300

    TASK13

---
+++ T13Program.cs
@@ -0,0 +1,16 @@
+﻿string str = Console.ReadLine();
+
+if (str.Length<3)
+{
+    Console.WriteLine("Third number does not exist");
+}
+else {
+    Console.WriteLine(Thirdnumber(str));
+}
+
+char Thirdnumber(string number)
+{
+    char num = number[2];
+    return num;
+
+}

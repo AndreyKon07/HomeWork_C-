9bd15fb2b507f5e3fd24f9db79b0cf893caf285f
AndreyKon07 <107915506+AndreyKon07@users.noreply.github.com>
2022-06-29 17:12:19 +0300

    TASK15

---
+++ T15Program.cs
@@ -0,0 +1,15 @@
+﻿void Dayofweek(int x)
+
+{
+    if ( x >= 1 &&  x < 6) Console.WriteLine("Work Day");
+    if ( x >= 6 &&  x <= 7) Console.WriteLine("Holliday");
+    if ( x < 1 ||  x > 7) Console.WriteLine("Please, input correct number");
+
+}
+
+Console.Write("Day's number: ");
+
+int x = Convert.ToInt32(Console.ReadLine());
+
+Dayofweek(x);
+

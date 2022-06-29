dd10fa6c8f9d5eb91b67605d440deba83da32c9d
AndreyKon07 <107915506+AndreyKon07@users.noreply.github.com>
2022-06-29 17:10:57 +0300

    TASK10

---
+++ T10Program.cs
@@ -0,0 +1,11 @@
+﻿int Secnumber()
+{
+    int ran = new Random().Next(100, 1000);
+    Console.WriteLine("Random number is: " + ran);
+    int dec = ran / 10;
+    int ed = dec %10;
+    return ed;
+
+}
+int sec = Secnumber();
+Console.WriteLine("Second number is: " + sec);

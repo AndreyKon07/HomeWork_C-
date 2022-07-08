d8584477a4fe6d9086aa95ceeec5178174116d40
AndreyKon07 <107915506+AndreyKon07@users.noreply.github.com>
2022-07-08 20:46:51 +0300

    TASK29

---
+++ T29Program.cs
@@ -0,0 +1,20 @@
+﻿int[] Newarr(int size, int min, int max)
+{
+    int[] array = new int[size];
+
+    for(int i=0; i<size; i++)
+        array[i]= new Random().Next(min, max +1);
+        return array;
+}
+
+void NewArr(int[] array)
+
+{
+    for(int i=0; i<array.Length; i++)
+    Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+
+int[] myarray=Newarr(8,1,9096560);
+NewArr(myarray);
